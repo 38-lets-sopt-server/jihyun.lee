38e0e75766f49e58ce89c0ae165a1d02dc0e54c9
Jyunee54 <jyunee54@gmail.com>
2026-05-22 19:08:49 +0900

    feat(#16): JwtAuthFilter 수정

--- src/main/java/org/sopt/config/JwtAuthFilter.java
+++ src/main/java/org/sopt/config/JwtAuthFilter.java
@@ -41,10 +41,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(auth);
             } catch (IllegalArgumentException | JWTVerificationException e) {
-                // 유효하지 않은 토큰 또는 토큰이 없는 경우, 인증 없이 다음 필터로 넘겨요.
-                // 여기서 예외를 던지지 않는 이유는, /v1/login 같이 인증이 필요 없는 API도
-                // 이 필터를 거치기 때문이에요. 인증 여부 판단은 SecurityConfig의
-                // authorizeHttpRequests 설정에서 담당합니다.
+                SecurityContextHolder.clearContext();
             }
         }
 
