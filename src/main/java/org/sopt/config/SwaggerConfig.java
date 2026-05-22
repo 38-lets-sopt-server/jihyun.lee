b86253b721d89ca6961dae9513c020752394ca2d
Jyunee54 <jyunee54@gmail.com>
2026-05-22 19:19:50 +0900

    feat(#16): Swagger 인증 추가

--- src/main/java/org/sopt/config/SwaggerConfig.java
+++ src/main/java/org/sopt/config/SwaggerConfig.java
@@ -1,18 +1,28 @@
 package org.sopt.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info().title("Swagger API").version("v1"))
-                .addServersItem(new Server().url("/"));
+                .addServersItem(new Server().url("/"))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 }
